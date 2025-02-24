68c4cf5503c39f171113c440b0d2c67ae4427e80
tdh <tdhster@gmail.com>
2025-02-24 14:14:14 +0800

    prodaction adoptation

--- app.py
+++ app.py
@@ -368,3 +368,7 @@ def privacy():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
+    
+# if __name__ == '__main__':
+#     from waitress import serve  # pip install waitress
+#     serve(app, host="0.0.0.0", port=5001)
